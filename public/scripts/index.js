ac3e520e0b3523bd0bb12dc57d3575985451fdb1
Synthird <105394020+Synthird@users.noreply.github.com>
2025-11-13 16:30:10 -0800

    Added a search bar

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,6 @@
 const emojiList = document.querySelector(".emoji-list"),
+	searchDiv = document.querySelector(".search-div"),
+	searchBar = document.getElementById("search-bar"),
 	loadingText = document.getElementById("loading-text");
 
 fetch("/api/emojis")
@@ -13,12 +15,29 @@ fetch("/api/emojis")
 		for (const name of Object.keys(emojis)) {
 			emojiList.innerHTML += `
 				<div>
-					<p>${name}</p>
+					<p class="emoji-name">${name}</p>
 					<p><img src=${emojis[name]}></p>
 				</div>
 			`;
 		}
 
 		loadingText.remove();
+		searchDiv.style.display = "block";
 	})
 	.catch(err => loadingText.innerHTML = `<p>${err}</p>`);
+
+searchBar.addEventListener("keyup", () => {
+	const searchBarValue = searchBar.value,
+		emojis = document.querySelectorAll(".emoji-name");
+	console.log(searchBarValue);
+
+	for (const emoji of emojis) {
+		const emojiContainer = emoji.parentElement;
+
+		if (!emoji.textContent.includes(searchBarValue.toLowerCase())) {
+			emojiContainer.style.display = "none";
+		} else {
+			emojiContainer.style.display = "block";
+		}
+	}
+});
