d439ba979f30c966dbbe1433760d73cf62558415
Synthird <105394020+synthird@users.noreply.github.com>
2026-07-31 21:35:51 -0700

    chore: revert back to commit d15c725

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import getEmojisRouter from "./routes/get-emojis-router.js";
 
-const app = express();
+export const app = express();
 app.use("/api/emojis", getEmojisRouter);
 
 // Running the site locally
