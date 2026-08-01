5f32b69f807ebb0984819615663e9bec5263b4ec
Synthird <105394020+synthird@users.noreply.github.com>
2026-07-31 21:21:27 -0700

    refactor: export app in server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import getEmojisRouter from "./routes/get-emojis-router.js";
 
-export const app = express();
+const app = express();
 app.use("/api/emojis", getEmojisRouter);
 
 // Running the site locally
 app.use(express.static("public"));
 app.listen(8080, () => console.log("View site at: http://localhost:8080"));
+
+export default app;
